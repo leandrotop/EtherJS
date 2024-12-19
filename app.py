6255828550508350ab52c9bffe921d8b97f3050a
leandrotop <62478436+leandrotop@users.noreply.github.com>
2024-12-19 14:36:49 -0300

    Update app.py
    Criando um host

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from flask import Flask, send_from_directory
 
-class WebView(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("WebView - Google")
+app = Flask(__name__)
 
-        # Configurando o layout
-        self.browser = QWebEngineView()
-        self.browser.setUrl("https://www.google.com")
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
 
-        # Adicionando o WebView à janela principal
-        container = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(self.browser)
-        container.setLayout(layout)
-        self.setCentralWidget(container)
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = WebView()
-    window.show()
-    sys.exit(app.exec_())
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
