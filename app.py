2d7c499410ed81a7e98621d61fdf8424c2e977cb
leandrotop <62478436+leandrotop@users.noreply.github.com>
2024-12-18 21:40:43 -0300

    Update app.py
    Teste web

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
-# script_teste.py
-print("Hello, Termux! Bem-vindo ao teste de script Python.")
-nome = input("Qual é o seu nome? ")
-print(f"Olá, {nome}! O script rodou com sucesso.")
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+class WebView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("WebView - Google")
+
+        # Configurando o layout
+        self.browser = QWebEngineView()
+        self.browser.setUrl("https://www.google.com")
+
+        # Adicionando o WebView à janela principal
+        container = QWidget()
+        layout = QVBoxLayout()
+        layout.addWidget(self.browser)
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = WebView()
+    window.show()
+    sys.exit(app.exec_())
