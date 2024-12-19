0c9e4900ca72d1d13c2971725d23b351dbd82262
leandrotop <62478436+leandrotop@users.noreply.github.com>
2024-12-19 14:44:23 -0300

    Update app.py
    Correção de bug

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory('.', 'Index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
